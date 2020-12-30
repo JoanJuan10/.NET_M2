c5362f9db76c92e309502c7e894d8bada14e6e8f
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-30 09:52:27 +0100

    Fase 1

---
+++ EjercicioM2/EjercicioM2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EjercicioM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] nom = { 'J', 'O', 'A', 'N' };
+
+            for (int i = 0; i < nom.Length; i++)
+            {
+                Console.Write(nom[i]);
+            }
+        }
+    }
+}
